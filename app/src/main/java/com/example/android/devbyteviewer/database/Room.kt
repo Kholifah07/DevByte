21aea6dfdc36571ed8728ee36360836f15f23f19
Kholifah07 <siti.nurkholifah@students.utdi.ac.id>
2022-12-09 14:16:29 +0700

    1

---
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+//digunakan untuk mendapatkan video dari data base
+@Dao
+// berfungsi sebagai database class object
+interface VideoDao {
+    // menampilkan data
+    //digunakan untuk mendapatkan data menghasilkan list data base video
+    @Query("select * from databasevideo")
+    // live data
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+    // digunakn untuk menambahkan data
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    // digunakan untuk menginstal semua video yang akan ditampilkan
+    fun insertAll( videos: List<DatabaseVideo>)
+}
+
+
+// merupakan kelas  DatabaseVideo merupakan Room
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+    // digunakan untuk mengakses method dao
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    VideosDatabase::class.java,
+                    "videos").build()
+        }
+    }
+    return INSTANCE
+}
